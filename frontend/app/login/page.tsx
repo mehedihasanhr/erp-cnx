d17f2309fba0de5286bf2ef40a42c1de89225475
Joy Debnath <96875662+developerjoydebnath@users.noreply.github.com>
2023-10-06 23:07:41 +0600

    login 2

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -66,7 +66,7 @@ export default function Login() {
                         <div>
                             <input
                                 type="submit"
-                                className="border w-full mt-5 py-2 rounded font-semibold text-white bg-green-500"
+                                className="border w-full mt-5 py-2 rounded font-semibold text-white bg-green-500 cursor-pointer"
                                 value="Login"
                             />
                         </div>
